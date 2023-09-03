01055612ff03f1d2632499a7a4f5ff976da7a544
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-03 17:11:03 -0600

    Initial Commit

---
+++ MetaIntroViewHierarchy/MetaIntroViewHierarchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaIntroViewHierarchyApp.swift
+//  MetaIntroViewHierarchy
+//
+//  Created by Mitch Andrade on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaIntroViewHierarchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
